7ad5f2123054d6269e2d43b72c38add7592fa901
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-06-11 13:00:02 -0700

    create HTML maze with room IDs in cells and exits in CSS classes

--- make_html_maps.py
+++ make_html_maps.py
@@ -9,14 +9,15 @@ def make_html_header(map):
     html = '<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n'
     html += '    <title>' + map + '</title>\n'
     html += '    <link rel="stylesheet" src="maze.css" />\n'
-    html += '</head>\n<body>'
+    html += '</head>\n<body>\n'
+    html += '    <h1>' + map + '</h1>\n    <table>\n'
 
     return html
 
 # create final part of HTML document
 def make_html_footer():
 
-    html = '</body>\n</html>'
+    html = '    </table>\n</body>\n</html>'
 
     return html
 
@@ -42,29 +43,72 @@ for map in maps:
         if room_col > largest_col:
             largest_col = room_col
 
-    print(map, largest_row, largest_col)
-
     # create an array to hold the map
     maze = []
-    row = [[]] * (largest_col + 1)
-
-    for _ in range(0, largest_row + 1):
-        maze.append(row)
     
-    # add all rooms to maze
+    for row_ID in range(0, largest_row + 1):
+        
+        row = [[]] * (largest_col + 1)
+        maze.append(row)
+        
+    # add all relevant room data to maze
+    # each entry in the grid will contain the ID and exits of the room
     for room_ID in room_graph:
         room_data = room_graph[room_ID]
 
         room_row, room_col = room_data[0]
 
-        # maze[room_row][room_col] = room_data
+        exits = []
 
-    print(maze)
+        if "n" in room_data[1]:
+            exits.append("n")
+        if "s" in room_data[1]:
+            exits.append("s")
+        if "e" in room_data[1]:
+            exits.append("e")
+        if "w" in room_data[1]:
+            exits.append("w")
 
-    # with open("maps/" + map + ".html", "w") as html:
+        maze[room_row][room_col] = {"id": room_ID, "exits": exits}
 
-    #     html.write(make_html_header(map))
-    #     html.write("test")
-    #     html.write(make_html_footer())
+    with open("maps/" + map + ".html", "w") as html:
+
+        html.write(make_html_header(map))
+
+        # add a header for column IDs
+        html.write('        <tr>\n            <td></td>\n')
         
-    #     print("Created " + map + ".html")
+        for col_ID in range(len(maze[0])):
+            html.write('            <td>' + str(col_ID) +'</td>\n')
+        
+        html.write('        </tr>\n')
+
+        # add data for each room
+        for row_ID in range(len(maze)):
+            
+            row = maze[row_ID]
+
+            html.write('        <tr>\n            <td>' + str(row_ID) +'</td>\n')
+
+            for col_ID in range(len(maze[0])):
+
+                room_data = row[col_ID]
+
+                # if there is data in the room, add it
+                if len(room_data) > 0:
+
+                    room_ID = room_data["id"]
+                    exits = " ".join(room_data["exits"])
+
+                    html.write('            <td class="' + exits + '">')
+                    html.write(str(room_ID))
+                    html.write("</td>\n")
+
+                else:
+                    html.write("            <td></td>\n")
+
+            html.write('        </tr>\n')
+
+        html.write(make_html_footer())
+        
+        print("Created " + map + ".html")
