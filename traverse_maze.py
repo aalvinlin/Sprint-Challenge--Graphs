01155dea3c5f19bcddcf2a3fccdee1bd83692355
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-06-12 08:46:04 -0700

    implement breadth-first search to find path between two nodes in graph

--- traverse_maze.py
+++ traverse_maze.py
@@ -1,9 +1,9 @@
 class Queue():
     def __init__(self):
         self.queue = []
-    def push(self, value):
+    def enqueue(self, value):
         self.queue.append(value)
-    def pop(self):
+    def dequeue(self):
         if self.size() > 0:
             return self.queue.pop(0)
         else:
@@ -55,6 +55,70 @@ class Graph:
 
     def get_neighbors(self, vertex_id):
         return self.vertices[vertex_id]
+
+    # use breadth-first search to return a list of nodes to traverse to travel between two vertices
+    def find_path_from(self, starting_vertex, destination_vertex):
+
+        # create a queue to hold vertices to traverse
+        vertices_to_visit = Queue()
+
+        # initialize queue with starting vertex
+        vertices_to_visit.enqueue(starting_vertex)
+
+        # use a dictionary to keep track of visited vertices and their path from the starting node
+        paths_to_vertices = dict()
+        paths_to_vertices[starting_vertex] = []
+
+        # use a set to keep track of visited vertices
+        vertices_already_visited = set()
+
+        while vertices_to_visit.size() > 0:
+
+            # get next vertex in line
+            current_vertex = vertices_to_visit.dequeue()
+
+            # process current vertex if it hasn't been visited yet
+            if current_vertex not in vertices_already_visited:
+
+                # mark current vertex as visited and store its path at the same time
+                vertices_already_visited.add(current_vertex)
+
+                # inspect all the neighbors of the current vertex
+                # neighbor_data = [neighbor for neighbor in self.get_neighbors(current_vertex) if neighbor is not None]
+                neighbor_data = self.get_neighbors(current_vertex)
+
+                for direction in neighbor_data:
+
+                    neighbor = neighbor_data[direction]
+
+                    # there are entries for all four directions, even if the neighbor doesn't exist
+                    # skip those neighbors if they don't exist
+                    if neighbor is not None:
+
+                        # if the target vertex is one of the neighbors, the search is done
+                        # right now paths_to_vertices[current_vertex] only contains all the vertices up to and including the parent vertex
+                        # to return the full path, add both the current vertex and the target vertex first.
+                        if neighbor == destination_vertex:
+                            final_path = paths_to_vertices[current_vertex][:]
+                            final_path.append(current_vertex)
+                            final_path.append(neighbor)
+                            return final_path
+
+                        # add all the other neighbors to the queue
+                        vertices_to_visit.enqueue(neighbor)
+
+                        # store a copy of the current path for each of the neighbors
+                        # take the path leading to current_vertex and add current_vertex to it
+                        # make a copy in order to not modify the original
+                        copy_of_path_to_parent = paths_to_vertices[current_vertex][:]
+                        copy_of_path_to_parent.append(current_vertex)
+
+                        # store path in dictionary
+                        paths_to_vertices[neighbor] = copy_of_path_to_parent
+        
+        # target not found
+        print("Vertex", destination_vertex, "was not found.")
+        return
 
 # use a dictionary to look up opposite directions (used for backtracking)
 opposite_directions = dict()
@@ -114,6 +178,8 @@ def traverse_maze(player):
                     print(new_room.id, "can't be reached from", previous_room.id)
 
                     # use breadth-first search to find a route
+                    # print("  ", maze.get_neighbors(new_room.id), maze.get_neighbors(previous_room.id), )
+                    print(maze.find_path_from(previous_room.id, new_room.id))
 
                     # add that route to traversal_path
 
